4db96b0a2b35ce158486e4ed7e827bf867159f0b
zerowhy <zerowhy.server@protonmail.com>
2022-03-12 20:24:18 -0300

    Adicionar classe Database

--- src/backend/database_interface.py
+++ src/backend/database_interface.py
@@ -1,8 +1,31 @@
 # Script para realizar a interface entre a GUI e a base de dados
 
+import sqlite3 as sql
 
 class Database:
+    def __init__(dbfile, silent=False):
+        # Inicializar Classe
+        self.dbfile = dbfile
+        self.silent = silent
+        create_connection()
+        
+
+    def create_connection():
+        # Tentar criar a conexão com a base de dados
+        conn = None;
+        try:
+            con = sql.connect(self.dbfile)
+            # Para razões de debugging
+            if not self.silent:
+                print(f"\nUsando SQLite3\nVersão: {sql.version}\n")
+        except Error as e:
+            print(e)
+        finally:
+            self.con = con
+            self.cur = con.cursor()
+    
 
 
+# Tratamento caso o script seja executado diretamente
 if __name__ == "__main__":
-    print("Esse programa nao foi feito para ser executado por si so, pois se trata de uma biblioteca.")
+    raise(Exception("ERRO\n\"database_interface.py\" não foi feito para ser executado por si só, pois se trata de uma biblioteca."))
