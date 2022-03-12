0effa1c0b88f872a87237d0e5019fa9745f30bdd
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-12 20:27:42 -0300

    fix database_interface.py

--- src/backend/database_interface.py
+++ src/backend/database_interface.py
@@ -2,30 +2,30 @@
 
 import sqlite3 as sql
 
+
 class Database:
-    def __init__(dbfile, silent=False):
+    def __init__(self, dbfile, silent=False):
         # Inicializar Classe
         self.dbfile = dbfile
         self.silent = silent
-        create_connection()
-        
+        self.create_connection()
 
-    def create_connection():
+    def create_connection(self):
         # Tentar criar a conexão com a base de dados
-        conn = None;
+        con = None
         try:
             con = sql.connect(self.dbfile)
             # Para razões de debugging
             if not self.silent:
                 print(f"\nUsando SQLite3\nVersão: {sql.version}\n")
-        except Error as e:
+        except Exception as e:
             print(e)
         finally:
             self.con = con
             self.cur = con.cursor()
-    
 
 
 # Tratamento caso o script seja executado diretamente
 if __name__ == "__main__":
-    raise(Exception("ERRO\n\"database_interface.py\" não foi feito para ser executado por si só, pois se trata de uma biblioteca."))
+    raise (Exception(
+        "ERRO\n\"database_interface.py\" não foi feito para ser executado por si só, pois se trata de uma biblioteca."))
