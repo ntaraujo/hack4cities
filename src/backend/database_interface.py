e8b550cf0dec4cb4e0d3bdc18331761d20007d00
zerowhy <zerowhy.server@protonmail.com>
2022-03-12 19:12:09 -0300

    Mudar estrutura para nova ideia

---
+++ src/backend/database_interface.py
@@ -0,0 +1,8 @@
+# Script para realizar a interface entre a GUI e a base de dados
+
+
+class Database:
+
+
+if __name__ == "__main__":
+    print("Esse programa nao foi feito para ser executado por si so, pois se trata de uma biblioteca.")
