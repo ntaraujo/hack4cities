846d161bde92fee32f108382b5b738e7ad38f705
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-13 19:40:13 -0300

    update max zoom

--- src/static/js/universal.js
+++ src/static/js/universal.js
@@ -1,7 +1,9 @@
 var map = L.map('map', {
     center: [-23.735422359974557, -46.58157182583245],
-    zoom: 100
+    zoom: 19
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxNativeZoom: 19,
+    maxZoom: 19
 }).addTo(map);
