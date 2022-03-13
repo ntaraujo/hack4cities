ab00c58ae7df060d59fe21f30c7168cb094d331c
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2022-03-13 00:24:26 -0300

    create map and open in google when double click

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    markers = [
+        {
+            'lat': -23.6921055,
+            'lon': -46.5536373,
+            'popup': 'This is the middle of the map.',
+            'color': 'red',
+            'fillColor': '#f03',
+            'fillOpacity': 0.5,
+            'radius': 500
+        }
+    ]
+    return render_template('index.html', markers=markers)
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8080, debug=True)
---
+++ src/static/js/universal.js
@@ -0,0 +1,7 @@
+var map = L.map('map', {
+    center: [-23.6921055, -46.5536373],
+    zoom: 10
+});
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
