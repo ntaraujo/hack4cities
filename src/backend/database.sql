9cc219575b66c87606d05b8ad2b41c93c6b18fc9
zerowhy <zerowhy.server@protonmail.com>
2022-03-13 10:14:53 -0300

    Adicionar arquivos de base de dados e melhorias na interface

--- src/backend/database.sql
+++ src/backend/database.sql
@@ -1,10 +1,4 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "vazao" (
-	"id"	INTEGER NOT NULL,
-	"tempo"	INTEGER NOT NULL UNIQUE,
-	"valor"	REAL NOT NULL,
-	FOREIGN KEY("id") REFERENCES "dispositivo"("id")
-);
 CREATE TABLE IF NOT EXISTS "dispositivo" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"cordx"	INTEGER NOT NULL,
@@ -13,5 +7,11 @@ CREATE TABLE IF NOT EXISTS "dispositivo" (
 	"idproximo"	INTEGER,
 	FOREIGN KEY("idproximo") REFERENCES "dispositivo",
 	PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "vazao" (
+	"id"	INTEGER NOT NULL,
+	"tempo"	INTEGER NOT NULL,
+	"valor"	REAL NOT NULL,
+	FOREIGN KEY("id") REFERENCES "dispositivo"("id")
 );
 COMMIT;
--- src/backend/database_interface.py
+++ src/backend/database_interface.py
@@ -28,6 +28,7 @@ class Database:
             print(e)
 
     def _create_database(self):
+        # Tentar inserir .sql na base de dados
         with open(self.schema, 'r') as sql_file:
             sql_script = sql_file.read()
             sql_file.close()
@@ -35,12 +36,29 @@ class Database:
             print("Inserindo tabelas na base de dados...")
         self.cur.executescript(sql_script)
 
-    def get_data(self):
-        pass
+    def get_data(self, time):
+        data = []
+        self.cur.execute("SELECT id, valor FROM vazao WHERE tempo=?", (time, ))
+        flow = self.cur.fetchall()
+        self
+        return flow
+    
+    def get_info(self, id):
+        self.cur.execute("SELECT cordx, cordy, instalacao, idproximo FROM dispositivo WHERE id=?", (id,))
+        info = self.cur.fetchone()
+        data = {"x": info[0], "y": info[1], "instalacao": info[2], "idproximo": info[3]}
+        return data
+        
+
+        
+
 
 
 # Tratamento caso o script seja executado diretamente
 if __name__ == "__main__":
-    db = Database()
-    #raise (Exception(
-    #    "ERRO\n\"database_interface.py\" não foi feito para ser executado por si só, pois se trata de uma biblioteca."))
+    #db = Database()
+    #print(db.get_data(1))
+    #print(db.get_info(1))
+    raise (Exception(
+        "ERRO\n\"database_interface.py\" não foi feito para ser executado por si só, pois se trata de uma biblioteca."))
+    
