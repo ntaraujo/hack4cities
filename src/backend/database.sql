8b9775d22208ed0be34a72e70bf263984fef4145
zerowhy <zerowhy.server@protonmail.com>
2022-03-12 21:25:25 -0300

    Adicionar arquivo .sql para base de dados

---
+++ src/backend/database.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "vazao" (
+	"id"	INTEGER NOT NULL,
+	"tempo"	INTEGER NOT NULL UNIQUE,
+	"valor"	REAL NOT NULL,
+	FOREIGN KEY("id") REFERENCES "dispositivo"("id")
+);
+CREATE TABLE IF NOT EXISTS "dispositivo" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"cordx"	INTEGER NOT NULL,
+	"cordy"	INTEGER NOT NULL,
+	"instalacao"	INTEGER,
+	"idproximo"	INTEGER,
+	FOREIGN KEY("idproximo") REFERENCES "dispositivo",
+	PRIMARY KEY("id")
+);
+COMMIT;
